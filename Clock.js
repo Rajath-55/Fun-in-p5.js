473919f5e48a1b8f2565dcf14198841ae06094b8
Rajath V <f20190122@goa.bits-pilani.ac.in>
2020-01-02 22:47:49 +0530

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -10,7 +10,8 @@ function draw() {
   let hr=hour();
   let mn=minute();
   let sc=second();
-  
+  let ms=millis();
+  let d=day();
  
   stroke(255,100,125);
   strokeWeight(6);
@@ -34,5 +35,28 @@ function draw() {
   rotate(end3);
   line(0,0,70,0);
   pop();
+  stroke(255);
+  strokeWeight(3);
+  
+  
+  
+  ellipse(0,60,30,30);
+  ellipse(0,-60,30,30);
+  ellipse(-65,0,50,50);
+  push();
+  stroke(255,130,200);
+  translate(-65,0);
+  
+  let msAngle=map(ms,0,100,0,360);
+  rotate(msAngle);
+  
+  line(0,0,15,0);
+  pop();
+  
+  push();
+  stroke(43,12,223);
+  translate(-45,60);
+  text(d,0,0);
+  
   
 }
